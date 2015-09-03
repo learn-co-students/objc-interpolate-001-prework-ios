35991a7458cce1d845726cc33e88574945c2c656
markedwardmurray <markedwardmurray@gmail.com>
2015-09-03 13:47:42 -0400

    first draft

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -2,16 +2,41 @@
 
 #import "FISAppDelegate.h"
 
-@interface FISAppDelegate ()
-
-@end
-
-
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    return YES;
+    
+    /*
+     
+     * Write your code here.
+     
+     */
+    
+    // A.
+    NSLog(@"Dalek");
+    NSLog(@"%@", @"Dalek");
+    NSLog(@"%@%@%@%@%@", @"D", @"a", @"l", @"e", @"k");
+
+    
+    // B.
+    NSLog(@"Interpolate!");
+    NSLog(@"%@", @"Interpolate!");
+    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
+    NSLog(@"%@%@%@%@!", @"In", @"ter", @"po", @"late");
+    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    
+    
+    // C.
+    NSLog(@"You are not The Doctor!");
+    NSLog(@"%@", @"You are not The Doctor!");
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
+    
+    // Do not alter
+    return YES;  //
+    //-------------
 }
 
 @end
