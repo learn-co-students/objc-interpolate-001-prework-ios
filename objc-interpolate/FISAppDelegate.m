2cd979204739592a5aa77c6efb36c46505931b4f
ariingber <ariingber@gmail.com>
2015-11-03 13:59:16 -0500

    first submission of interpolation lab (lab 2)

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -7,12 +7,25 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    //Section A
+    NSLog(@"Dalek");
+    NSLog(@"%@",@"Dalek");
+    NSLog(@"%@%@%@%@%@",@"D",@"a",@"l",@"e",@"k");
     
+    //Section B
+    NSLog(@"Interpolate!");
+    NSLog(@"%@", @"Interpolate!");
+    NSLog(@"%@%@", @"Interpolate!", @"Interpolate!");
+    NSLog(@"%@%@%@%@!",@"In",@"ter",@"po",@"late");
+    NSLog(@"%@-%@-%@-%@!",@"In",@"ter",@"po",@"late");
+    
+    //Section C
+    NSLog(@"You are not The Doctor!");
+    NSLog(@"%@",@"You are not The Doctor!");
+    NSLog(@"%@%@%@%@%@%@",@"You",@"are",@"not",@"The",@"Doctor",@"!");
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
+          
     // Do not alter
     return YES;  //
     ///////////////
