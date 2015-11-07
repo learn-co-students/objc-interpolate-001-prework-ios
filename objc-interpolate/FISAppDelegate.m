55672bcb800a989eceac886088232515c8bf7901
Michelle vargas <michellevargas@Homes-iMac.local>
2015-11-07 17:36:32 -0500

    completed lab

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -12,6 +12,40 @@
      * Write your code here!
      
      */
+   
+    
+    NSLog(@"Dalek");
+    
+    NSLog(@"%@",@"Dalek");
+    
+    NSLog(@"%@%@%@%@%@", @"D", @"a", @"l", @"e", @"k");
+    
+    NSLog(@"InterPolate!");
+    
+    NSLog(@"%@",@"Interpolate!");
+    
+    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
+    
+    
+    NSLog(@"%@%@%@%@!",@"In",@"ter",@"po",@"late");
+    
+    NSLog(@"%@-%@-%@-%@!", @"In",@"ter",@"po",@"late");
+    
+    
+    NSLog(@"You are not the Doctor!");
+    
+    NSLog(@"%@",@"You are not the Doctor");
+    
+    NSLog(@"%@%@%@%@%@%@",@"You" ,@"are" ,@"not" ,@"The" ,@"Doctor",@"!");
+    
+    NSLog(@"You are%@%@!", @"not", @"The Doctor");
+          
+    
+    
+    
+
+    
+    
     
     // Do not alter
     return YES;  //
