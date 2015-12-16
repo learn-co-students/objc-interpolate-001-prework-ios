8826b5cf9e74a77579e611f22f7918771213c336
abcheromar <abcheromar@gmail.com>
2015-12-16 16:13:24 -0500

    Done with the strings

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -7,11 +7,29 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /*
-     
-     * Write your code here!
-     
-     */
+    NSLog(@"Dalek");
+    NSLog(@"%@",  @"Dalek");
+    
+    NSLog( @"%@%@%@%@%@",  @"D", @"a", @"l", @"e", @"k");
+    
+    
+    
+    NSLog(@"Interpolate!");
+    NSLog(@"%@", @"Interpolate!");
+    
+    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
+    NSLog(@"%@%@%@%@!", @"In", @"ter", @"po", @"late");
+    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    NSLog(@"You are not The Doctor!");
+    NSLog(@"%@", @"You are not The Doctor!");
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
+
+
+
+
     
     // Do not alter
     return YES;  //
