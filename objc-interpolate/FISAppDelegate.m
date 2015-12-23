9b7a23c6a415905f163e341696a025ba395d5feb
susanlovaglio <susan.lovaglio@gmail.com>
2015-12-23 12:04:05 -0500

    done

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -12,7 +12,19 @@
      * Write your code here!
      
      */
-    
+    NSLog(@"Dalek");
+    NSLog(@"%@", @"Dalek");
+    NSLog(@"%@%@%@%@%@", @"D", @"a", @"l", @"e",@"k");
+    NSLog(@"Interpolate!");
+    NSLog(@"%@",@"Interpolate!");
+    NSLog(@"%@ %@", @"Interpolate!" , @"Interpolate!");
+    NSLog(@"%@%@%@%@!", @"In", @"ter",@"po",@"late");
+    NSLog(@"%@-%@-%@-%@!",@"In",@"ter",@"po",@"late");
+    NSLog(@"You are not the doctor!");
+    NSLog(@"%@", @"You are not the doctor!");
+    NSLog(@"%@%@%@%@%@%@",@"You",@"are",@"not",@"the",@"doctor",@"!");
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You",@"are",@"not",@"the",@"doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not",@"the doctor");
     // Do not alter
     return YES;  //
     ///////////////
