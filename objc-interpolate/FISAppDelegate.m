b1eea422cd7a700abfed7f832b09eb38d7007472
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-04 10:59:35 -0500

    Completes tasks for "C. You are not The Doctor!"

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -13,12 +13,12 @@
      
      */
     
-    // B.
-    NSLog(@"Interpolate!");
-    NSLog(@"%@", @"Interpolate!");
-    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
-    NSLog(@"%@%@%@%@!", @"In", @"ter", @"po", @"late");
-    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    // C.
+    NSLog(@"You are not The Doctor!");
+    NSLog(@"%@", @"You are not The Doctor!");
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
     
     // Do not alter
     return YES;  //
