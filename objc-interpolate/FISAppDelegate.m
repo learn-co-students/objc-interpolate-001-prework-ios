a53e93f8e5d664dcdacf4d3502d41b66d6eb7853
sam3killa <samuels.shih@gmail.com>
2016-01-05 15:47:01 -0800

    Added some NSLogging

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -13,6 +13,38 @@
      
      */
     
+    // Normal String
+    NSLog(@"Dalek");
+    // String Interpolation
+    NSLog(@"%@",@"Dalek");
+    // More String Interpolation
+    NSLog(@"%@%@%@%@%@",@"D",@"A",@"L",@"E",@"K");
+    
+    
+    // More Interpolation
+    NSLog(@"Interpolate");
+    NSLog(@"%@",@"Interpolate");
+    NSLog(@"%@,%@",@"Interpolate",@"Interpolate");
+    NSLog(@"%@%@%@%@",@"In",@"ter",@"pol",@"ate");
+    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    
+    // More Interpolation
+    NSLog(@"You are not The Doctor!");
+    NSLog(@"%@", @"You are not The Doctor!");
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
     // Do not alter
     return YES;  //
     ///////////////
