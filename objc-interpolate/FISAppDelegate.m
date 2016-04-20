9e4522bad657bdb8223638eef59be7730f26fd36
jellybeanjohnny <mamerige@icloud.com>
2016-04-20 07:31:03 -0400

    Adding nslog statements

--- objc-interpolate/FISAppDelegate.m
+++ objc-interpolate/FISAppDelegate.m
@@ -13,6 +13,32 @@
      
      */
     
+    
+    NSLog(@"Dalek");
+    
+    NSLog(@"%@", @"Dalek");
+    
+    NSLog(@"%@%@%@%@%@", @"D", @"a", @"l", @"e", @"k");
+    
+    NSLog(@"Interpolate!");
+    
+    NSLog(@"%@", @"Interpolate");
+    
+    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
+    
+    NSLog(@"%@%@%@%@", @"In", @"ter", @"po", @"late");
+    
+    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    
+    NSLog(@"You are not The Doctor!");
+    
+    NSLog(@"%@", @"You are not The Doctor!");
+    
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+
+    NSLog(@"You are %@ %@!", @"not", @"The Doctor");
     // Do not alter
     return YES;  //
     ///////////////
